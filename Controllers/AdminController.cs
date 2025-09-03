ce5868d2cf19dd1203b661f477a55ba500b0ce3a
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2025-09-03 14:51:55 +0700

    beauty home page

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace FarewellMyBeloved.Controllers;
 
@@ -22,9 +23,64 @@ public class AdminController : Controller
 
     [Authorize(Policy = "AdminsOnly")]
     [HttpGet("")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var viewModel = new AdminIndexViewModel();
+        
+        // Generate Farewell People chart data
+        await GenerateChartDataAsync<FarewellPerson>(viewModel.FarewellPeopleChartData,
+            fp => fp.CreatedAt);
+        
+        // Generate Farewell Messages chart data
+        await GenerateChartDataAsync<FarewellMessage>(viewModel.FarewellMessagesChartData,
+            fm => fm.CreatedAt);
+        
+        return View(viewModel);
+    }
+    
+    private async Task GenerateChartDataAsync<T>(ChartDataViewModel chartData,
+        Func<T, DateTime> dateSelector) where T : class
+    {
+        var now = DateTime.Now;
+        var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Asia/Jakarta");
+        var nowJakarta = TimeZoneInfo.ConvertTimeFromUtc(now.ToUniversalTime(), timeZoneInfo);
+        
+        // Get all data and filter client-side
+        var allData = await _context.Set<T>().ToListAsync();
+        
+        // Last 7 days data
+        for (int i = 6; i >= 0; i--)
+        {
+            var date = nowJakarta.AddDays(-i);
+            var startOfDay = TimeZoneInfo.ConvertTimeToUtc(date.Date, timeZoneInfo);
+            var endOfDay = startOfDay.AddDays(1);
+            
+            var count = allData.Count(e => dateSelector(e) >= startOfDay && dateSelector(e) < endOfDay);
+                
+            chartData.Last7DaysLabels.Add(date.ToString("dd/MM"));
+            chartData.Last7DaysData.Add(count);
+        }
+        
+        // Last 4 weeks data
+        for (int i = 3; i >= 0; i--)
+        {
+            var weekStart = nowJakarta.AddDays(-(i * 7 + 6));
+            var weekEnd = weekStart.AddDays(7);
+            
+            var startOfWeek = TimeZoneInfo.ConvertTimeToUtc(weekStart.Date, timeZoneInfo);
+            var endOfWeek = TimeZoneInfo.ConvertTimeToUtc(weekEnd.Date, timeZoneInfo);
+            
+            var count = allData.Count(e => dateSelector(e) >= startOfWeek && dateSelector(e) < endOfWeek);
+                
+            var weekNumber = GetWeekNumber(weekStart);
+            chartData.Last4WeeksLabels.Add($"Week {weekNumber}");
+            chartData.Last4WeeksData.Add(count);
+        }
+    }
+    
+    private int GetWeekNumber(DateTime date)
+    {
+        return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
     }
 
     [Authorize(Policy = "AdminsOnly")]
---
+++ ViewModels/AdminIndexViewModel.cs
@@ -0,0 +1,15 @@
+namespace FarewellMyBeloved.ViewModels;
+
+public class AdminIndexViewModel
+{
+    public ChartDataViewModel FarewellPeopleChartData { get; set; } = new();
+    public ChartDataViewModel FarewellMessagesChartData { get; set; } = new();
+}
+
+public class ChartDataViewModel
+{
+    public List<string> Last7DaysLabels { get; set; } = new();
+    public List<int> Last7DaysData { get; set; } = new();
+    public List<string> Last4WeeksLabels { get; set; } = new();
+    public List<int> Last4WeeksData { get; set; } = new();
+}
